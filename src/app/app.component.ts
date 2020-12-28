6f269d6a040d4424fc2de95b5af959f1e4dfba2f
Senayit <luckilycharmed@gmail.com>
2020-12-28 14:54:26 -0600

    more steps added

---
+++ src/app/app.component.ts
@@ -0,0 +1,48 @@
+import { Satellite} from './satellite';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'orbit-report';
+  displayList: Satellite [];
+  sourceList: Satellite[]; 
+  constructor() {
+    this.sourceList = [];
+    this.displayList = [];
+    let satellitesUrl = 'https://handlers.education.launchcode.org/static/satellites.json';
+ 
+    window.fetch(satellitesUrl).then(function(response) {
+       response.json().then(function(data) {
+ 
+          let fetchedSatellites = data.satellites;
+          for (let i = 0; i < fetchedSatellites.length; i++) {
+            this.sourceList.push(new Satellite(fetchedSatellites[i].name, fetchedSatellites[i].type, fetchedSatellites[i].launchDate, fetchedSatellites[i].orbitType, fetchedSatellites[i].operational))
+         }
+         this.displayList = this.sourceList.slice(0);
+      }.bind(this));
+   }.bind(this));
+     
+ }
+
+search(searchTerm: string): void {
+  let matchingSatellites: Satellite[] = [];
+  searchTerm = searchTerm.toLowerCase();
+  for (let i = 0; i < this.sourceList.length; i++) {
+     let name = this.sourceList[i].name.toLowerCase();
+     let type = this.sourceList[i].type.toLowerCase();
+     let orbitType = this.sourceList[i].orbitType.toLowerCase();
+     if (name.indexOf(searchTerm) >= 0) {
+        matchingSatellites.push(this.sourceList[i]);
+     } else if (type.indexOf(searchTerm) >= 0) {
+        matchingSatellites.push(this.sourceList[i]);
+     } else if (orbitType.indexOf(searchTerm) >= 0) {
+        matchingSatellites.push(this.sourceList[i]);
+     }
+  }
+  this.displayList = matchingSatellites;
+}
+}

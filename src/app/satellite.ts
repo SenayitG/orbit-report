94ae7f83b3ab6a14309379aec6d9dfea5fc48fbb
Senayit <luckilycharmed@gmail.com>
2020-12-28 20:58:49 -0600

    finishing touches

--- src/app/satellite.ts
+++ src/app/satellite.ts
@@ -1,17 +1,18 @@
+//import { findLast } from '@angular/compiler/src/directive_resolver';
+
 export class Satellite {
     name: string;
     orbitType: string;
     type: string;
     operational: boolean;
     launchDate: string;
-  
-
+    
     constructor(name: string, type: string, launchDate: string, orbitType: string, operational: boolean){
         this.name = name;
         this.type = type;
         this.launchDate = launchDate;
         this.orbitType = orbitType;
-        this.operational = operational;   
+        this.operational = operational;
     }
 
     shouldShowWarning(): boolean {
